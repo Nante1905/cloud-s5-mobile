17f56effc4847a1ea5cc31088122e50bf85a24a1
Tiavina-irintsoa <rakotonirinairintsoa0@gmail.com>
2024-02-04 23:14:32 +0300

    add [ADDANNONCE] integration estimation prix

--- src/components/service/Add-annonce.service.ts
+++ src/components/service/Add-annonce.service.ts
@@ -1,4 +1,5 @@
 import { http } from "../../shared/service/interceptor/axios.interceptor";
+import { Voiture } from "../../shared/types/creation-annonce-types";
 export const getAllMarque  = ()=> http.get(`/marques`);
 export const getAllModele  = ()=> http.get(`/modeles`);
 export const getAllEtat  = ()=> http.get(`/etats`);
@@ -6,3 +7,4 @@ export const getAllCouleur  = ()=> http.get(`/couleurs`);
 export const getModeleByMarque = (marque:number)=>http.get(`modeles/fromMarque/`+marque);
 export const getAllEnergie = ()=> http.get(`energies`);
 export const getAllBV = ()=> http.get(`vitesses`)
+export const estimate = (voiture: Voiture)=> http.post(`annonces/estimate`,voiture)
